be8628865e783805b1e765082948a114500b51d8
Soren's Coding Land <chaz.com@hotmail.co.uk>
2018-06-24 12:32:52 +0100

    Lecture 84 - Using Cross Platform Input - Done.
    This was a good referesher in cross platform input.

---
+++ Cheese 3D Shooter Project/Cheese 3D Shooter/Assets/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // Throw is whether it is pushed in one direction or the other. So -horizontal or just horizontal.
+        print(horizontalThrow);
+	}
+}
