7dec83ab491316f31eb35bf4ac9d2cf4df1abe55
Soren's Coding Land <chaz.com@hotmail.co.uk>
2018-06-27 18:17:07 +0100

    Lecture 85 - Input Sensitivity & Gravity - Done.
    I finished the lecture and it took all afternoon. The challenge took a while but it was good to go through this some more.

--- Cheese 3D Shooter Project/Cheese 3D Shooter/Assets/Player.cs
+++ Cheese 3D Shooter Project/Cheese 3D Shooter/Assets/Player.cs
@@ -5,12 +5,18 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f; // does the same as public but he likes using it throughout the 2.0 course. 
+                                                             // He sets it to 4 rather than 1f inthe inspector like I did.
+                                                             // I don't know what ms^-1 means but I think it's a shorthand for the calculaion we have done.
+
 	void Start () {
-		
+
 	}
 	
 	void Update () {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // Throw is whether it is pushed in one direction or the other. So -horizontal or just horizontal.
-        print(horizontalThrow);
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // Throw is whether it is pushed in one direction or the other. So -horizontal or just horizontal.
+        float xOffsetThisFrame = xThrow * xSpeed * Time.deltaTime;
+        print(xOffsetThisFrame);
 	}
+
 }
