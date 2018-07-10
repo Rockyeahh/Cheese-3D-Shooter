f0ebca0fb23f5a61e856fffceb1f4f316642995c
Soren's Coding Land <chaz.com@hotmail.co.uk>
2018-07-10 21:32:37 +0100

    Lecture 102: Instantiating At Runtime
    It was a good little refresher with instantiating at runtime and parenting object at run time.

--- Cheese 3D Shooter Project/Cheese 3D Shooter/Assets/Scripts/Enemy.cs
+++ Cheese 3D Shooter Project/Cheese 3D Shooter/Assets/Scripts/Enemy.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+
 	void Start () { // Maybe Awake would be better.
         Collider boxCollider = gameObject.AddComponent<BoxCollider>();
         boxCollider.isTrigger = false;
@@ -12,7 +15,9 @@ public class Enemy : MonoBehaviour {
 
     void OnParticleCollision(GameObject other)
     {
-        Destroy(gameObject);
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity); // original object, world space position, Don't rotate.
+        fx.transform.parent = parent; // Sets the parent transform to the fx variable that contains the instance. Thus putting it on parent, which is the Spawned At Run Time GameObject.
+        Destroy(gameObject); // Does it respawn after a Player death and Player respawn/reset?
     }
     
 }
